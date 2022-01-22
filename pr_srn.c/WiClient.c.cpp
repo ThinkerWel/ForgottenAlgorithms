f87985f5043d3fdb4df6524a8cd9f33afc22cff8
Ruslan <92936147+Ruslandsdsdsdsdwsd@users.noreply.github.com>
2022-01-22 18:03:27 +0300

    Add files via upload

---
+++ pr_srn.c/WiClient.c.cpp
@@ -0,0 +1,91 @@
+﻿#pragma comment(lib, "ws2_32.lib")
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#include <winsock2.h>
+#include <iostream>
+#include <stdio.h>
+#pragma warning(disable: 4996)
+#include <string>
+
+SOCKET Connection;
+
+enum  Packet 
+{
+	P_ChatMessage
+};
+
+bool ProcessPacket(Packet packettype) // обработка приходящий пакетов
+{
+	switch (packettype) {// обработка типа пакета 
+	case P_ChatMessage:
+	{
+		int msg_size;
+		recv(Connection, (char*)&msg_size, sizeof(int), NULL);
+		char* msg = new char[msg_size + 1];
+		msg[msg_size] = '\0';
+		recv(Connection, msg, msg_size, NULL);
+		printf("%s\n", msg);
+		delete[] msg;
+		break;
+	}
+	default:
+		printf("no connected packet\n");
+		break;
+	}
+	return true;
+}
+
+void Hendre()
+{
+	Packet packettype;
+	while (true) {
+		recv(Connection, (char*)&packettype, sizeof(Packet), NULL);
+		if (!ProcessPacket(packettype))
+		{
+			break;
+		}
+	}
+
+	closesocket(Connection);
+}
+
+int main(int argc, char* argv[]) {
+
+	WSAData wsaData;
+	WORD Lounge = MAKEWORD(2, 1);
+	if (WSAStartup(Lounge, &wsaData) != 0) {
+		printf("Error #1 %d\n", WSAGetLastError());
+		exit(1);
+	}
+
+	SOCKADDR_IN addr;
+	int sizeofaddr = sizeof(addr);
+	addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	addr.sin_port = htons(1111);
+	addr.sin_family = AF_INET;
+
+
+	 Connection = socket(AF_INET, SOCK_STREAM, NULL);
+	if (connect(Connection, (SOCKADDR*)&addr, sizeof(addr)) != 0) {
+		printf( "Error.\n");
+		return 1;
+	}
+	printf("Connect!!!\n");
+	
+	CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)Hendre, NULL, NULL,NULL);
+
+	std::string msg2;
+
+	while (true)
+	{
+		std::getline(std::cin, msg2);
+		int msg_size = msg2.size();// размер строки 
+		Packet packettype = P_ChatMessage;
+		send(Connection, (char*)&packettype, sizeof(Packet), NULL); 
+		send(Connection, (char*)&msg_size, sizeof(int), NULL); // посылаем строку серверу 
+		send(Connection, msg2.c_str(), msg_size, NULL); // посылаем строку 
+
+		Sleep(10);
+	}
+	system("pause");
+	return 0;
+}
---
+++ pr_srn.c/WinSock1.c.cpp
@@ -0,0 +1,106 @@
+﻿#pragma warning(disable: 4996)
+#pragma comment(lib, "ws2_32.lib")
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#include <winsock2.h>
+#include <stdio.h>
+#include <string>
+
+SOCKET Connect[100];
+int Counter = 0;
+
+enum  Packet
+{
+	P_ChatMessage
+};
+
+bool ProcessPacket(int index, Packet packettype)
+{
+	switch (packettype) {
+	case P_ChatMessage:
+	{
+		int msg_size;
+		recv(Connect[index], (char*)&msg_size, sizeof(int), NULL);
+		char* msg = new char[msg_size + 1];
+		msg[msg_size] = '\0';
+		recv(Connect[index], msg, msg_size, NULL);
+		for (int i = 0; i < Counter; i++) {
+			if (i == index) {
+				continue;
+			}
+			Packet msgtype = P_ChatMessage;
+			send(Connect[i], (char*)&msgtype, sizeof(Packet), NULL);
+			send(Connect[i], (char*)&msg_size, sizeof(int), NULL);
+			send(Connect[i], msg, msg_size, NULL);
+		}
+		delete[] msg;
+		break;
+	}
+	default:
+		printf("No packet #2 !!!");
+		break;
+	}
+	return true;
+}
+
+void Sclient(int index) {
+	Packet packettype;
+	
+	while (true) {
+		recv(Connect[index], (char*)&packettype, sizeof(Packet), NULL);
+		if (!ProcessPacket(index, packettype)) {
+			break;
+		}
+
+	}
+	closesocket(Connect[index]); 
+	
+}
+
+int main(int argc, char* argv[]) {
+	
+	WSAData wsaData;
+	WORD DLLVersion = MAKEWORD(2, 1);
+	if (WSAStartup(DLLVersion, &wsaData) != 0) {
+		printf("Error #1 %d\n", WSAGetLastError());
+		exit(1);
+	}
+
+	SOCKADDR_IN addr;
+	int sizeofaddr = sizeof(addr);
+	addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	addr.sin_port = htons(1111);
+	addr.sin_family = AF_INET;
+
+	SOCKET sListen = socket(AF_INET, SOCK_STREAM, NULL);
+	bind(sListen, (SOCKADDR*)&addr, sizeof(addr));
+	listen(sListen, SOMAXCONN);
+	
+
+	SOCKET Client;
+	for (int i = 0; i < 100; i++) {
+		Client = accept(sListen, (SOCKADDR*)&addr, &sizeofaddr);
+
+		if (Client == 0) {
+			printf("Error #2\n");
+		}
+		else {
+			printf( "Client Connected!\n");
+			std::string msg = "Hello Client,i m Conencting!";
+			int msg_size = msg.size();
+
+			Packet msgtype = P_ChatMessage;
+			send(Client, (char*)&msgtype, sizeof(Packet), NULL);
+
+			send(Client, (char*)&msg_size, sizeof(int), NULL);
+			send(Client, msg.c_str(), msg_size, NULL);
+
+			Connect[i] = Client;
+			Counter++;
+			CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)Sclient, (LPVOID)(i), NULL, NULL); 
+
+		}
+	}
+	
+	system("pause");
+	return 0;
+}
