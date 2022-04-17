d7f8459c6f35c26523b2bfec3e5659abde86fdcb
Ruslan <92936147+Ruslandsdsdsdsdwsd@users.noreply.github.com>
2022-04-17 23:29:50 +0300

    Update RegisTr.c

--- RegisTr.c
+++ RegisTr.c
@@ -19,3 +19,40 @@ char per(char *str)
     printf("%s",str);
     return str;
 }
+
+
+
+// удаляет все гласные буквы в строке ! //
+
+char *disemvowel(const char *str)
+{
+  char *ans = malloc(sizeof(char) * strlen(str) + 1);
+  int j = 0;
+  for(int i = 0; i < strlen(str); ++i){
+    switch(str[i]){
+      case 'a':
+      case 'e':
+      case 'i':
+      case 'o':
+      case 'u':
+      case 'A':
+      case 'E':
+      case 'I':
+      case 'O':
+      case 'U':
+        break;
+      default:
+        ans[j++] = str[i]; 
+    }
+  }
+   ans[j] = '\0';
+printf("N= %s",ans);
+  return ans;
+}
+int main(void)
+{
+    char str[100];
+    scanf("%s",str);
+    disemvowel(&str);
+}
+
